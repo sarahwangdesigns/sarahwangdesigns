ab9e45b2abef6326a2194461ad02c9ed574cf3e0
sarahwangdesigns <sarwang@ucdavis.edu>
2017-10-05 14:43:58 -0700

    up

--- works/script.js
+++ works/script.js
@@ -65,13 +65,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
       img.src=url;
   }
 
-  $(document).ready(function() {
-   $("#myCarousel").swiperight(function() {
-      $(this).carousel('prev');
-    });
-   $("#myCarousel").swipeleft(function() {
-      $(this).carousel('next');
-   });
-});
+  
 
 });
